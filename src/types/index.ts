4902a31146003bccec417c6ba6077c01de79f66b
Olayinka Ola <mimoyinka@gmail.com>
2022-04-28 13:50:02 +0100

    Fork: React-Native-Paystack-Webview

---
+++ src/types/index.ts
@@ -0,0 +1,52 @@
+import type React from "react";
+
+export type Currency = 'NGN' | 'GHS' | 'USD' | 'ZAR';
+
+export type PaymentChannels = 'bank' | 'card' | 'qr' | 'ussd' | 'mobile_money';
+
+interface Response {
+  status: string;
+}
+interface SuccessResponse extends Response {
+  transactionRef?: string;
+  data?: any;
+}
+
+export interface PayStackProps {
+  paystackKey: string;
+  billingEmail: string;
+  firstName?: string;
+  lastName?: string;
+  phone?: string | number;
+  amount: string | number;
+  currency?: Currency;
+  channels?: PaymentChannels[];
+  refNumber?: string;
+  billingName?: string;
+  handleWebViewMessage?: (string: string) => void;
+  onCancel: (Response: Response) => void;
+  onSuccess: (SuccessResponse:SuccessResponse) => void;
+  autoStart?: boolean;
+  activityIndicatorColor?: string;
+  ref: React.ReactElement;
+}
+
+export interface ISplitPayment {
+  subAccount: string;
+  transactionCharge?: string | number;
+  bearer?: string
+}
+
+export interface IMultiSplitPayment {
+  splitCode: string;
+}
+
+export interface ISubscription {
+  plan: string
+  quantity?: string;
+}
+
+export interface PayStackRef {
+  startTransaction: () => void;
+  endTransaction: () => void;
+}
